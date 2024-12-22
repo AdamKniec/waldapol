9d637975cb06978175e5d9a424cdb0a384dd8c4b
AdamKniec <adam.k.kniec@gmail.com>
2024-12-22 18:38:11 +0100

    adding paddings and layout improvements to each sections

--- src/components/sections/Cooperation/Cooperation.tsx
+++ src/components/sections/Cooperation/Cooperation.tsx
@@ -2,16 +2,21 @@ import styles from "./Cooperation.module.css";
 
 export const Cooperation = () => {
   return (
-    <section>
+    <section className={styles.section}>
       <div className={styles.innerWrapper}>
-        <h2>Merchendising</h2>
-        <p>Zapraszamy do współpracy firmy z Polski i zagranicy</p>
-        <p>Oferujemy wyjątkowo korzystne warunki współpracy.</p>
-        <p>
-          Oferowane przez nas zbiorniki paliwa oraz nasze bogate doświadczenie i
-          ugruntowana pozycja na rynku sprawiają, że nasza firma jest solidnym
-          partnerem w biznesie.
-        </p>
+        <div className={styles.textWrapper}>
+          <h2>Merchendising</h2>
+          <p>Zapraszamy do współpracy firmy z Polski i zagranicy</p>
+          <p>Oferujemy wyjątkowo korzystne warunki współpracy.</p>
+          <p>
+            Oferowane przez nas zbiorniki paliwa oraz nasze bogate doświadczenie
+            i ugruntowana pozycja na rynku sprawiają, że nasza firma jest
+            solidnym partnerem w biznesie.
+          </p>
+        </div>
+        <div>
+          <img />
+        </div>
       </div>
     </section>
   );
