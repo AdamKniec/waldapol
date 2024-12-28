59c28ffddff9714e2696f114e876db9258d3535c
AdamKniec <adam.k.kniec@gmail.com>
2024-12-28 19:53:19 +0100

    adding fonts and some more css styles

--- src/components/sections/Cooperation/Cooperation.tsx
+++ src/components/sections/Cooperation/Cooperation.tsx
@@ -1,3 +1,4 @@
+import { BarlowCondensedFont800 } from "@/app/fonts";
 import styles from "./Cooperation.module.css";
 
 export const Cooperation = () => {
@@ -6,13 +7,19 @@ export const Cooperation = () => {
       <div className={styles.innerWrapper}>
         <div className={styles.textWrapper}>
           <h2>Merchendising</h2>
-          <p>Zapraszamy do współpracy firmy z Polski i zagranicy</p>
-          <p>Oferujemy wyjątkowo korzystne warunki współpracy.</p>
-          <p>
-            Oferowane przez nas zbiorniki paliwa oraz nasze bogate doświadczenie
-            i ugruntowana pozycja na rynku sprawiają, że nasza firma jest
-            solidnym partnerem w biznesie.
+          <p className={`${BarlowCondensedFont800.className} ${styles.header}`}>
+            Zapraszamy do współpracy firmy z Polski i zagranicy
           </p>
+          <div className={styles.innerTextWrapper}>
+            <p className={`${styles.paragraphRegular} ${styles.spacing}`}>
+              Oferujemy wyjątkowo korzystne warunki współpracy.
+            </p>
+            <p className={`${styles.paragraphRegular} ${styles.spacing}`}>
+              Oferowane przez nas zbiorniki paliwa oraz nasze bogate
+              doświadczenie i ugruntowana pozycja na rynku sprawiają, że nasza
+              firma jest solidnym partnerem w biznesie.
+            </p>
+          </div>
         </div>
         <div>
           <img />
--- src/components/sections/OfferIntro/OfferIntro.tsx
+++ src/components/sections/OfferIntro/OfferIntro.tsx
@@ -1,3 +1,4 @@
+import { BarlowCondensedFont800 } from "@/app/fonts";
 import styles from "./OfferIntro.module.css";
 
 export const OfferIntro = () => {
@@ -5,29 +6,35 @@ export const OfferIntro = () => {
     <section className={styles.sectionWrapper}>
       <div className={styles.innerWrapper}>
         <div>
-          <h2>
+          <h2
+            className={`${BarlowCondensedFont800.className} ${styles.header}`}
+          >
             Oferujemy zbiorniki paliwa do wszystkich typów samochodów
             ciężarowych
           </h2>
           <button>zamów teraz</button>
         </div>
-        <div>
-          <p>
+        <div className={styles.textWrapper}>
+          <p className={`${styles.textBold} ${styles.spacing}`}>
             Jesteśmy od lat na rynku polskim strategicznym partnerem handlowym
             głównych producentów oryginalnych zbiorników paliwowych.
           </p>
-          <p>
-            WALDAPOL to firma o ugruntowanej pozycji w sektorze zaopatrzenia w
-            części zamienne i zbiorniki paliwa do takich marek jak MAN, DAF,
-            VOLVO, IVECO, MERCEDES, SCANIA, RENAULT i inne.
+          <p className={styles.spacing}>
+            <span className={styles.paragraphRegular}>
+              WALDAPOL to firma o ugruntowanej pozycji w sektorze zaopatrzenia w
+              części zamienne i zbiorniki paliwa do takich marek jak
+            </span>
+            <span className={styles.textBold}>
+               MAN, DAF, VOLVO, IVECO, MERCEDES, SCANIA, RENAULT i inne.
+            </span>
           </p>
-          <p>
+          <p className={`${styles.paragraphRegular} ${styles.spacing}`}>
             Posiadamy w ofercie pełen asortyment litrażowy i gabarytowy
             zbiorników oraz wszystkie akcesoria niezbędne do montażu i
             podłączenia zbiornika takie jak podpory, obejmy, korki, zawory
             spustowe itd.
           </p>
-          <p>
+          <p className={styles.paragraphRegular}>
             Oferowane przez nas zbiorniki paliwa są najwyższej jakości, nasze
             doświadczenie w tej dziedzinie gwarantuje Państwu wysoką jakość i
             rzetelność obsługi.
