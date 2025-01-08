4c474c2af84d85b7f8056acb1659f8955933dc1f
AdamKniec <adam.k.kniec@gmail.com>
2025-01-08 20:02:35 +0100

    addding map iframe and stylistic improvements

--- src/components/sections/ContactForm/ContactForm.tsx
+++ src/components/sections/ContactForm/ContactForm.tsx
@@ -1,18 +1,18 @@
 import { BarlowCondensedFont500, BarlowCondensedFont800 } from "@/app/fonts";
 import styles from "./ContactForm.module.css";
-import Image from "next/image";
-import MapImge from "@/assets/images/map.png";
 
 export const ContactForm = () => {
   return (
     <section>
       <div className={styles.innerWrapper}>
         <div className={styles.imageWrapper}>
-          <Image
-            src={MapImge}
-            alt="Mapa przedstawiająca położenie firmy Waldapol"
-            height={1058}
-            width={588}
+          <iframe
+            src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Kopanica Spacerowa 6&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+            width="588"
+            height="1058"
+            style={{ border: 0 }}
+            aria-hidden="false"
+            className={styles.map}
           />
         </div>
         <div className={styles.mainContentWrapper}>
